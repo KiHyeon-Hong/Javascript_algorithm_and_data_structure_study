1db786c1b64e3f2bbb62c8241611cb58104bbed6
Ki Hyeon Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2022-01-08 14:53:00 +0900

    quick Sort?

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -1,5 +1,38 @@
-function mergeSort(arr) {}
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
 
 let data = Array.apply(null, { length: 100000 }).map((v) => {
   return parseInt(Math.random() * 100000);
 });
+
+// console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3], 0, 8));
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
